738ffb4a26846328ef8ccf479ce4cd81bbfbab9c
Linh Pham <lpham@smith.edu>
2023-02-14 22:32:50 -0500

    new changes

--- Conversation.java
+++ Conversation.java
@@ -9,7 +9,6 @@ class Conversation {
         int rounds = input.nextInt();
 
         List<String> transcript = new ArrayList<>();
-
         String opening = "Hi there! What's on your mind?";
         System.out.println(opening);
         transcript.add("Bot: " + opening);
@@ -17,45 +16,60 @@ class Conversation {
         String users_input = input.nextLine();
         transcript.add("You: " + users_input);
 
-        // Split the message into words
-        String[] words = users_input.split(" ");
-
-        
         int i = 0;
         while (i < rounds){
-            if (containsPronouns(users_input)==false){
             String next_input = input.nextLine();
+            String[] words = next_input.split(" ");
             transcript.add("You: " + next_input);
 
+            if (containsPronouns(next_input)==false){
             String response = getCannedResponse(random);
             System.out.println(response);
             transcript.add("Bot: " + response);
+       
             }
             else{
-            String next_input = input.nextLine();
-            transcript.add("You: " + next_input);
+            String nex_input = input.nextLine();
+            transcript.add("You: " + nex_input);
 
-            // Mirror the words based on a pre-defined dictionary
-            String[] mirroredWords = new String[words.length];
             for (int j = 0; j < words.length; j++) {
-            String mirroredWord = mirrorWord(words[j]);
-            mirroredWords[j] = mirroredWord;
-            System.out.println(mirroredWords[j]);
+                String word = words[j];
+    
+                if (word.equalsIgnoreCase("I")) {
+                    words[j] = "you";
+                    String mirroredSentence = String.join(" ", words);
+                    System.out.println(mirroredSentence + "?");
+                    transcript.add("Bot: " + mirroredSentence + "?");
+                } else if (word.equalsIgnoreCase("you")) {
+                    words[j] = "I";
+                    String mirroredSentence = String.join(" ", words);
+                    System.out.println(mirroredSentence + "?");
+                    transcript.add("Bot: " + mirroredSentence + "?");
+                } else if (word.equalsIgnoreCase("me")) {
+                    words[j] = "you";
+                    String mirroredSentence = String.join(" ", words);
+                    System.out.println(mirroredSentence + "?");
+                    transcript.add("Bot: " + mirroredSentence + "?");
+                } else if (word.equalsIgnoreCase("your")) {
+                    words[j] = "my";
+                    String mirroredSentence = String.join(" ", words);
+                    System.out.println(mirroredSentence + "?");
+                    transcript.add("Bot: " + mirroredSentence + "?");
+                }
+                
             }
-            
-            String response = String.join(" ", mirroredWords);
-            System.out.println(response);
-            transcript.add("Bot: " + response);
-            }
+        }
         i++;
+    }
+            System.out.println("Goodbye!");
+            System.out.println("Here's the transcript of our conversation:");
+            for (String line : transcript) {
+                if (line.isEmpty()==false) {
+                    System.out.println(line);
+
         }
-        System.out.println("Goodbye!");
-        System.out.println("Here's the transcript of our conversation:");
-        for (String line : transcript) {
-            if (line.isEmpty()==false) {
-                System.out.println(line);
-            }
-        }
+        
+    }
     }
         
         public static String getCannedResponse(Random random) {
@@ -77,35 +91,13 @@ class Conversation {
 
         public static boolean containsPronouns(String input) {
             // This method returns true if the input contains pronouns.
-            return input.contains("\\b" + "I" + "\\b")
-                || input.contains("\\b" + "me" + "\\b")
-                || input.contains("\\b" + "am" + "\\b")
-                || input.contains("\\b" + "you" + "\\b")
-                || input.contains("\\b" + "my" + "\\b")
-                || input.contains("\\b" + "your" + "\\b");
+            return input.contains("I")
+                || input.contains("me")
+                || input.contains("am")
+                || input.contains("you")
+                || input.contains("my")
+                || input.contains("your");
         }
-
-
-        private static String mirrorWord(String word) {
-            switch (word) {
-                case "I":
-                    return "you";
-                case "you":
-                    return "I";
-                case "my":
-                    return "your";
-                case "your":
-                    return "my";
-                case "am":
-                    return "are";
-                case "are":
-                    return "am";
-                default:
-                    return word;
-            }
-        }
-
-        
 
     
   }
