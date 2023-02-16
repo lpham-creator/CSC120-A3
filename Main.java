a41994f373b2156b7fad5cf3db3ccb4cbf4ac171
Linh Pham <lpham@smith.edu>
2023-02-15 23:35:13 -0500

    Minor changes

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@
    Filename: Main.java
 Description: Academic project A3 for the class CSC 120: Object Oriented Programming, prof. R. Jordan Crouser. File main to execute chatbot.
        Date: 15 Feb 2023
-
 */
 /* YOU DO NOT NEED TO MODIFY THIS FILE */
 public class Main {
